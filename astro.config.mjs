93e8f691810bd9517fb8e95fa0a4ca0154d6f3c8
Pietro Jomini <pietro.jomini@protonmail.ch>
2024-06-01 00:19:58 +0200

    gh pages deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,7 @@ import icon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [tailwind(), icon()]
+    integrations: [tailwind(), icon()],
+    site: 'https://leallodole.github.io',
+    base: '/www'
 });
